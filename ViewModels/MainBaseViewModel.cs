ba212d1c8549a5e4dfbafca17c00551c653c5c24
justixz <22521461@gm.uit.edu.vn>
2023-12-06 21:54:13 +0700

    Bind ContentControl to User Controls (Views), Installed MaterialDesign Toolkit

---
+++ ViewModels/MainBaseViewModel.cs
@@ -0,0 +1,39 @@
+﻿using CineMajestic.Views;
+using CineMajestic.Views.MovieManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CineMajestic.ViewModels
+{
+    public abstract class MainBaseViewModel : BaseViewModel
+    {
+        private object currentView;
+        public object CurrentView
+        {
+            get { return currentView; }
+            set { currentView = value; OnPropertyChanged(nameof(CurrentView)); }
+        }
+
+        public ICommand SwitchViewCommand { get; private set; }
+
+        public MainBaseViewModel()
+        {
+            SwitchViewCommand = new ViewModelCommand(SwitchView);
+        }
+
+        private void SwitchView(object userControlName)
+        {
+            string UserControlName = userControlName as string;
+            switch (UserControlName)
+            {
+                case "Movies":
+                    CurrentView = new MovieManagementView();
+                    break;
+            }
+        }
+    }
+}
---
+++ ViewModels/MovieManagementViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineMajestic.ViewModels
+{
+    public class MovieManagementViewModel : MainBaseViewModel
+    {
+        private string title;
+        public string Title { get => title; set { title = value; OnPropertyChanged(nameof(Title)); } }
+
+        public MovieManagementViewModel() 
+        {
+            
+        }
+    }
+}
