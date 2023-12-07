4ef46659ab97630d5ab1fd9776d6cc27a48da5dc
Trần Quỳnh Thy <146901553+tqthy@users.noreply.github.com>
2023-12-07 16:49:51 +0700

    Update CinemaManagement.sql

--- CinemaManagement.sql
+++ CinemaManagement.sql
@@ -20,3 +20,44 @@ CREATE TABLE EMPLOYEES(
     Account_id INT,
     CONSTRAINT FK_Account FOREIGN KEY (Account_id) REFERENCES ACCOUNTS(id)
 );
+GO
+INSERT INTO ACCOUNTS VALUES('admin', 'Admin@123', 1);
+GO
+CREATE TABLE MOVIES(
+    id INT PRIMARY KEY IDENTITY(1, 1),
+    Title NVARCHAR(100),
+    Description NVARCHAR(500),
+    Director NVARCHAR(50),
+    ReleaseDate DATE,
+    Language NVARCHAR(20),
+    Country NVARCHAR(20),
+    Length INT,
+    Trailer NVARCHAR(200),
+    StartDate SMALLDATETIME,
+    EndDate SMALLDATETIME,
+);
+GO
+CREATE TABLE GENRES(
+    id INT PRIMARY KEY IDENTITY(1, 1),
+    Title NVARCHAR(50)
+);
+GO
+CREATE TABLE MOVIES_GENRES(
+    id INT PRIMARY KEY IDENTITY(1, 1),
+    Movie_id INT REFERENCES MOVIES(id),
+    Genre_id INT REFERENCES GENRES(id)
+)
+GO
+INSERT INTO MOVIES VALUES(N'Bố Già', 
+                        N'Phim sẽ xoay quanh lối sống thường nhật của một xóm lao động nghèo, ở đó có bộ tứ anh em Giàu - Sang - Phú - Quý với Ba Sang sẽ là nhân vật chính, hay lo chuyện bao đồng nhưng vô cùng thương con cái. Câu chuyện phim tập trung về hai cha con Ba Sang (Trấn Thành) và Quắn (Tuấn Trần). Dù yêu thương nhau nhưng khoảng cách thế hệ đã đem đến những bất đồng lớn giữa hai cha con. Liệu cả hai có thể cho nhau cơ hội thấu hiểu đối phương, thu hẹp khoảng cách và tạo nên hạnh phúc từ sự khác biệt?',
+                        N'Trấn Thành',
+                        '2021/03/12',
+                        'VN',
+                        'VN',
+                        128,
+                        'https://www.youtube.com/watch?v=uVa1lTvmVhs',
+                        '2023/01/01',
+                        '2023/12/30');         
+GO              
+INSERT INTO GENRES VALUES (N'Gia Đình'), (N'Hài');
+GO
