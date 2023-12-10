9bd536729fa39e51e1baed21cd822e2d9d51d72b
Vu Binh <22520129@gm.uit.edu.vn>
2023-12-10 23:54:18 +0700

    Tạo Staff DTO

--- Models/DTOs/StaffManagement/StaffDTO.cs
+++ Models/DTOs/StaffManagement/StaffDTO.cs
@@ -8,5 +8,14 @@ namespace CineMajestic.Models.DTOs.StaffManagement
 {
     public class StaffDTO
     {
+        public string Id { get; set; }
+        public string FullName { get; set; }
+        public string DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Salary { get; set; }
+        public string Role { get; set; }
+        public string Account_id { get; set; }
     }
 }
