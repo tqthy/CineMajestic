ba621e7e1ada70c19ba1f8f607827d96febecbec
Dung Tien The <22520291@gm.uit.edu.vn>
2023-12-12 04:18:13 +0700

    QLNhanSu: Test UI (phần hiển thị danh sách nhân viên) -Phần Model: Xây dựng lớp StaffDA với dữ liệu mẫu(chưa connect DB) -Phần ViewModel: Sửa lại class StaffManageVM

---
+++ Models/DataAccessLayer/StaffDA.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CineMajestic.Models.DTOs.StaffManagement;
+
+namespace CineMajestic.Models.DataAccessLayer
+{
+    public class StaffDA
+    {
+        public static ObservableCollection<StaffDTO> getDSNV()
+        {
+            ObservableCollection<StaffDTO>list = new ObservableCollection<StaffDTO>();
+            list.Add(new StaffDTO(1, "Nguyễn Văn A", "2002", "Nam", "A@gmail.com", "090988777", 10000000, 1));
+            list.Add(new StaffDTO(2, "Nguyễn Văn B", "2002", "Nam", "b@gmail.com", "090988777", 20000000, 1));
+            list.Add(new StaffDTO(3, "Nguyễn Văn C", "2002", "Nam", "A@gmail.com", "090988777", 10000000, 1));
+            list.Add(new StaffDTO(4, "Nguyễn Văn D", "2002", "Nam", "A@gmail.com", "090988777", 10000000, 1));
+            list.Add(new StaffDTO(5, "Nguyễn Văn E", "2002", "Nam", "A@gmail.com", "090988777", 10000000, 1));
+            list.Add(new StaffDTO(6, "Nguyễn Văn Fddddddddddddddddddddddddddddddddddddddddd", "2002", "Nam", "A@gmail.com", "090988777", 10000000, 1));
+            list.Add(new StaffDTO(7, "Nguyễn Văn G", "2002", "Nam", "A@gmail.com", "090988777", 10000000, 1));
+            list.Add(new StaffDTO(8, "Nguyễn Văn A", "2002", "Nam", "A@gmail.com", "090988777", 10000000, 1));
+            list.Add(new StaffDTO(9, "Nguyễn Văn B", "2002", "Nam", "b@gmail.com", "090988777", 20000000, 1));
+            list.Add(new StaffDTO(10, "Nguyễn Văn C", "2002", "Nam", "A@gmail.com", "090988777", 10000000, 1));
+            list.Add(new StaffDTO(11, "Nguyễn Văn D", "2002", "Nam", "A@gmail.com", "090988777", 10000000, 1));
+            list.Add(new StaffDTO(12, "Nguyễn Văn E", "2002", "Nam", "A@gmail.com", "090988777", 10000000, 1));
+            list.Add(new StaffDTO(13, "Nguyễn Văn Fddddddddddddddddddddddddddddddddddddddddd", "2002", "Nam", "A@gmail.com", "090988777", 10000000, 1));
+            list.Add(new StaffDTO(14, "Nguyễn Văn G", "2002", "Nam", "A@gmail.com", "090988777", 10000000, 1));
+            list.Add(new StaffDTO(15, "Nguyễn Văn A", "2002", "Nam", "A@gmail.com", "090988777", 10000000, 1));
+            list.Add(new StaffDTO(16, "Nguyễn Văn B", "2002", "Nam", "b@gmail.com", "090988777", 20000000, 1));
+            list.Add(new StaffDTO(17, "Nguyễn Văn C", "2002", "Nam", "A@gmail.com", "090988777", 10000000, 1));
+            list.Add(new StaffDTO(18, "Nguyễn Văn D", "2002", "Nam", "A@gmail.com", "090988777", 10000000, 1));
+            list.Add(new StaffDTO(19, "Nguyễn Văn E", "2002", "Nam", "A@gmail.com", "090988777", 10000000, 1));
+            list.Add(new StaffDTO(20, "Nguyễn Văn Fddddddddddddddddddddddddddddddddddddddddd", "2002", "Nam", "A@gmail.com", "090988777", 10000000, 1));
+            list.Add(new StaffDTO(21, "Nguyễn Văn G", "2002", "Nam", "A@gmail.com", "090988777", 10000000, 1)); ;
+            return list;
+        }
+    }
+}
--- ViewModels/StaffManagementVM/StaffManageVM.cs
+++ ViewModels/StaffManagementVM/StaffManageVM.cs
@@ -1,33 +1,56 @@
-﻿using CineMajestic.Views.StaffManagement;
+﻿using CineMajestic.Models.DTOs.StaffManagement;
+using CineMajestic.Views.StaffManagement;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using CineMajestic.Models.DataAccessLayer;
 
 namespace CineMajestic.ViewModels.StaffManagementVM
 {
     public class StaffManageVM:BaseViewModel 
     {
-        public ICommand OpenStaffAddCommand { get; set; }
+       
+        private ObservableCollection<StaffDTO> dsnv;
+        public ObservableCollection<StaffDTO> DSNV
+        {
+            get => dsnv;
+            set
+            {
+                if (dsnv != value)
+                {
+                    dsnv = value;
+                    OnPropertyChanged(nameof(DSNV));
+                }
+            }
+        }
+
         public StaffManageVM()
         {
-          //  OpenStaffAddCommand = new ViewModelCommand(OpenStaffAdd);
-            StaffAdd();
+            DSNV = new ObservableCollection<StaffDTO>();
+            DSNV=StaffDA.getDSNV();
         }
-
-        void StaffAdd()
-        {
-            OpenStaffAddCommand = new ViewModelCommand(OpenStaffAdd);
-        }
-
-        private void OpenStaffAdd(object obj)
-        {
-            StaffAddView staffAdd = new StaffAddView();
-            staffAdd.ShowDialog();
-        }
-        
         
     }
 }
+
+//public ICommand OpenStaffAddCommand { get; set; }
+//public StaffManageVM()
+//{
+//  //  OpenStaffAddCommand = new ViewModelCommand(OpenStaffAdd);
+//    StaffAdd();
+//}
+
+//void StaffAdd()
+//{
+//    OpenStaffAddCommand = new ViewModelCommand(OpenStaffAdd);
+//}
+
+//private void OpenStaffAdd(object obj)
+//{
+//    StaffAddView staffAdd = new StaffAddView();
+//    staffAdd.ShowDialog();
+//}
