9bae511e32c28eb62306aa062bc8c2780a768cc3
Dung Tien The <22520291@gm.uit.edu.vn>
2023-12-12 14:02:11 +0700

    ProductManagement: Xây dựng button add ảnh ở wd EditProduct

--- ViewModels/ProductManagement/Feature/EditProductViewModel.cs
+++ ViewModels/ProductManagement/Feature/EditProductViewModel.cs
@@ -11,6 +11,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Media;
 
 namespace CineMajestic.ViewModels.ProductManagement
 {
@@ -34,20 +35,70 @@ namespace CineMajestic.ViewModels.ProductManagement
 
     public class EditProductViewModel:INotifyPropertyChanged
     {
-       
+        public string Name { get; set; }
+
+        private string imageSource;
+        public string? ImageSource
+        {
+            get => imageSource;
+            set
+            {
+                if (imageSource != value)
+                {
+                    imageSource = value;
+                    OnPropertyChanged(nameof(ImageSource));
+                }
+            }
+        }
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+        public int Type { get; set; }
+
         EditProduct wd;//phục vụ việc đóng wd
 
         public ICommand quitCommand { get; set; }//thoát k sửa product nữa
+        public ICommand addImageCommand {  get; set; }//đồng ý edit
+
     
         public EditProductViewModel(EditProduct wd)
         {
-            quitCommand = new ViewModelCommand(quit);     
+            quitCommand = new ViewModelCommand(quit);  
+            addImageCommand=new ViewModelCommand(addImage);
             this.wd = wd;
         }
 
+
+        //thoát(hủy k edit nữa)
         private void quit(object obj)
         {
             wd.Close();
+        }
+
+
+
+        //add image
+        private void addImage(object obj)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Ảnh (*.jpg, *.jpeg, *.png)|*.jpg;*.jpeg;*.png";
+
+            bool? result = openFileDialog.ShowDialog();
+
+            if (result == true)
+            {
+                string selectedImagePath = openFileDialog.FileName;
+                string fileName = Path.GetFileName(selectedImagePath);
+                try
+                {
+                    MotSoPhuongThucBoTro.copyFile(selectedImagePath, MotSoPhuongThucBoTro.pathProject() + @"Images\ProductManagement");
+                    ImageSource = MotSoPhuongThucBoTro.pathProject() + @"Images\ProductManagement\" + fileName;
+                }
+                catch
+                {
+                    MessageBox.Show("Vui lòng đổi lại tên ảnh,file ảnh bạn chọn có tên trùng với nơi lưu trữ dữ liệu!");
+                    wd.Close();
+                }
+            }
         }
 
         public event PropertyChangedEventHandler? PropertyChanged;
