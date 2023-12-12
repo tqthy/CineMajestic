52c2e58796c6099ab8c41034b5132e9a17122183
Vu Binh <22520129@gm.uit.edu.vn>
2023-12-12 11:15:21 +0700

    Chỉnh sửa connecstring để test UI

--- Models/DataAccessLayer/DataAccess.cs
+++ Models/DataAccessLayer/DataAccess.cs
@@ -12,7 +12,9 @@ namespace CineMajestic.Models.DataAccessLayer
         private readonly string _connectionString;
         public DataAccess()
         {
-            _connectionString = "Server=tcp:192.168.0.199,1433;Database=CinemaManagement;User ID=sa;Password=Password.1";
+            //_connectionString = "Server=tcp:192.168.0.199,1433;Database=CinemaManagement;User ID=sa;Password=Password.1";
+            //Dùng tạm để  test ở local
+            _connectionString = @"Data Source=LAPTOP-J7OECDJF\SQLEXPRESS;Initial Catalog=CinemaManagement;Integrated Security=True";
         }
         protected SqlConnection GetConnection()
         {
