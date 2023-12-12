42d5a4d9b5bb418cd3d58f6721a2e101fac2a2da
Dung Tien The <22520291@gm.uit.edu.vn>
2023-12-12 11:12:24 +0700

    ProductManagement: Xây dựng command để show wd EditProduct

---
+++ ViewModels/ProductManagement/Feature/EditProductViewModel.cs
@@ -0,0 +1,28 @@
+﻿using CineMajestic.Views.ProductManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CineMajestic.ViewModels.ProductManagement
+{
+    public partial class ProductManagementViewModel
+    {
+        public ICommand showWDEditProductCommand { get; set; }
+
+        void editProduct()
+        {
+            showWDEditProductCommand = new ViewModelCommand(ShowWDEditProduct);
+        }
+        private void ShowWDEditProduct(object obj)
+        {
+            EditProduct editProduct = new EditProduct();
+            editProduct.ShowDialog();
+
+            //load lại danh sách
+            loadData();
+        }
+    }
+}
--- ViewModels/ProductManagement/ProductManagementViewModel.cs
+++ ViewModels/ProductManagement/ProductManagementViewModel.cs
@@ -43,6 +43,7 @@ namespace CineMajestic.ViewModels.ProductManagement
             PhanLoai();
             addProduct();
             delete();
+            editProduct();
         }
 
     }
