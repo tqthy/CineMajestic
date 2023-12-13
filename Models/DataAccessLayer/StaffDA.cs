cd51f69ad3f9f91723aa7cb51b91927641fd2696
Vu Binh <22520129@gm.uit.edu.vn>
2023-12-13 15:57:27 +0700

    Fix lõi căn lề trong UI

--- Models/DataAccessLayer/StaffDA.cs
+++ Models/DataAccessLayer/StaffDA.cs
@@ -13,7 +13,9 @@ namespace CineMajestic.Models.DataAccessLayer
         public static ObservableCollection<StaffDTO> getDSNV()
         {
             ObservableCollection<StaffDTO>list = new ObservableCollection<StaffDTO>();
-            list.Add(new StaffDTO(1, "Nguyễn Văn A", "2002", "Nam", "A@gmail.com", "090988777", 10000000, "Quản lí","2000/10/01"));
+            list.Add(new StaffDTO(1, "Nguyễn Văn A", "24/8/2002", "Nam", "A@gmail.com", "090988777", 10000000, "Quản lí", "2000/10/01"));
+            list.Add(new StaffDTO(2, "Nguyễn Văn B", "1/2/2002", "Nam", "B@gmail.com", "090988777", 10000000, "Nhân viên", "2000/10/01"));
+            list.Add(new StaffDTO(3, "Nguyễn Văn C", "3/5/2002", "Nam", "C@gmail.com", "090988777", 10000000, "Quản lí","2000/10/01"));
             return list;
         }
     }
