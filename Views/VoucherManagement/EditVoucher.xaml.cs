1581ec4c0237b0f42ac6d7d2b4db366c039cc168
Vu Binh <22520129@gm.uit.edu.vn>
2023-12-14 09:13:58 +0700

    Hoàn thành UI cho edit và add vouvher view

---
+++ Views/VoucherManagement/EditVoucher.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CineMajestic.Views.VoucherManagement
+{
+    /// <summary>
+    /// Interaction logic for EditVoucher.xaml
+    /// </summary>
+    public partial class EditVoucher : Window
+    {
+        public EditVoucher()
+        {
+            InitializeComponent();
+        }
+    }
+}
