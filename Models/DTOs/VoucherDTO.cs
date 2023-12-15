2483311a535df3ff8196c28e8a086c151defbf63
Dung Tien The <22520291@gm.uit.edu.vn>
2023-12-15 19:14:06 +0700

    Voucher: Xây dựng class VoucherDTO

---
+++ Models/DTOs/VoucherDTO.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Xaml.Behaviors.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineMajestic.Models.DTOs
+{
+    public class VoucherDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string VoucherDetail {  get; set; }
+        public string Type {  get; set; }
+        public string StartDate {  get; set; }
+        public string FinDate {  get; set; }
+
+        public VoucherDTO()
+        {
+
+        }
+
+        public VoucherDTO(int id, string name, string code, string voucherDetail, string type, string startDate, string finDate)
+        {
+            Id = id;
+            Name = name;
+            Code = code;
+            VoucherDetail = voucherDetail;
+            Type = type;
+            StartDate = startDate;
+            FinDate = finDate;
+        }
+
+        public VoucherDTO(string name, string code, string voucherDetail, string type, string startDate, string finDate)
+        {
+            Name = name;
+            Code = code;
+            VoucherDetail = voucherDetail;
+            Type = type;
+            StartDate = startDate;
+            FinDate = finDate;
+        }
+
+    }
+}
