c438a73f47f58dabd4823783e2f46ce6f313d8f9
Dung Tien The <22520291@gm.uit.edu.vn>
2023-12-15 10:21:30 +0700

    QLNhanSu: hoàn thành chức năng add Staff (riêng phần password chưa mã hóa)

--- ViewModels/StaffManagementVM/Feature/AddStaffViewModel.cs
+++ ViewModels/StaffManagementVM/Feature/AddStaffViewModel.cs
@@ -7,6 +7,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using CineMajestic.Models.DataAccessLayer;
+using CineMajestic.Models.DTOs;
 using CineMajestic.Models.DTOs.StaffManagement;
 using CineMajestic.Views.StaffManagement;
 using MaterialDesignThemes.Wpf;
@@ -31,21 +32,27 @@ namespace CineMajestic.ViewModels.StaffManagementVM
         }
     }
 
-    public class AddStaffViewModel : BaseViewModel
+    public class AddStaffViewModel : MainBaseViewModel
     {
 
-        private StaffAddView wd;//phục vụ quit
-        public string FullName {  get; set; }
+        private StaffAddView wd;//phục vụ quit và add 
+
+
+        //add cho bảng Staff
+        public string FullName { get; set; }
         public string Gender { get; set; }
-        public DateTime?Birth {  get; set; }
-        public string Email {  get; set; }
-        public string PhoneNumber {  get; set; }
-        public string Role {  get; set; }
-        public DateTime? NgayVL {  get; set; }
-        public int Salary {  get; set; }
+        public DateTime? Birth { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Role { get; set; }
+        public DateTime? NgayVL { get; set; }
+        public int Salary { get; set; }
+
+        //add bảng account
+        public string Username { get; set; }
 
         public ICommand CancelCommand { get; set; }
-        public ICommand acceptAddCommand {  get; set; }//đồng ý thêm
+        public ICommand acceptAddCommand { get; set; }//đồng ý thêm
 
 
         public AddStaffViewModel(StaffAddView wd)
@@ -62,7 +69,7 @@ namespace CineMajestic.ViewModels.StaffManagementVM
         //đồng ý thêm 
         private void accpetAdd(object obj)
         {
-            //sau này nhớ xử lý lỗi người dùng k nhập gì,nhập sai
+            ////sau này nhớ xử lý lỗi người dùng k nhập gì,nhập sai
             if (Gender == null)
             {
                 MessageBox.Show("Vui lòng chọn giới tính");
@@ -73,19 +80,33 @@ namespace CineMajestic.ViewModels.StaffManagementVM
                 MessageBox.Show("Vui lòng chọn chức vụ");
                 return;
             }
-            if(Birth == null)
+            if (Birth == null)
             {
                 MessageBox.Show("Vui lòng chọn ngày sinh");
                 return;
             }
-            if(NgayVL == null)
+            if (NgayVL == null)
             {
                 MessageBox.Show("Vui lòng chọn ngày vào làm");
                 return;
             }
+            if (wd.txtMatKhau.Password != wd.txtNhapLaiMatKhau.Password)
+            {
+                MessageBox.Show("2 mật khẩu không trùng nhau");
+                return;
+            }
 
+            //nhớ xử lý ràng buộc unique account rồi mới add staff
+            //cách đẹp nhất là truy vấn bảng account rồi xét
+
+            //add vào bảng Staff
             StaffDA staffDA = new StaffDA();
             staffDA.addStaff(new StaffDTO(FullName, Birth.Value.ToString("yyyy-MM-dd"), Gender, Email, PhoneNumber, Salary, Role, NgayVL.Value.ToString("yyyy-MM-dd")));
+
+            //add vào bảng Account,sau này nhớ mã hóa pass ở đây luôn
+            UserDA userDA = new UserDA();
+            userDA.addAccount(new UserDTO(Username, wd.txtMatKhau.Password, staffDA.identCurrent()));
+
             MessageBox.Show("Thêm nhân viên thành công");
             wd.Close();
 
