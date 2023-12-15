45370cad0beb24f877a0592540ef52754ed83fed
Dung Tien The <22520291@gm.uit.edu.vn>
2023-12-15 19:09:20 +0700

    VoucherManagement: xây dựng hàm tạo voucher

---
+++ ViewModels/VoucherManagement/MotSoPTBoTro.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineMajestic.ViewModels.VoucherManagement
+{
+    public class MotSoPTBoTro
+    {
+
+        //hàm tạo voucher
+        public static string createCode()
+        {
+            Random random = new Random();
+
+
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            StringBuilder stringBuilder = new StringBuilder(10);
+
+            for (int i = 0; i < 10; i++)
+            {
+                stringBuilder.Append(chars[random.Next(chars.Length)]);
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
