5b60c067b267009bfeb9d5b9009a3c17e07e9d6d
Dung Tien The <22520291@gm.uit.edu.vn>
2023-12-18 22:01:36 +0700

    ForgetPassword: Test

--- Models/DataAccessLayer/DataAccess.cs
+++ Models/DataAccessLayer/DataAccess.cs
@@ -12,7 +12,8 @@ namespace CineMajestic.Models.DataAccessLayer
         private readonly string _connectionString;
         public DataAccess()
         {
-            _connectionString = "Server=tcp:192.168.0.199,1433;Database=CinemaManagement;User ID=sa;Password=Password.1";
+            //_connectionString = "Server=tcp:192.168.0.199,1433;Database=CinemaManagement;User ID=sa;Password=Password.1";
+            _connectionString = @"Data Source=DESKTOP-34OSP4G\SQLEXPRESS;Initial Catalog=QL_RapChieuPhim;Integrated Security=True";
         }
         protected SqlConnection GetConnection()
         {
