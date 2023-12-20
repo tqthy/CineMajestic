8071978f29394ca767156e4eecd6d8b19a59ce61
Dung Tien The <22520291@gm.uit.edu.vn>
2023-12-20 19:55:08 +0700

    QLNhanSu: Hoàn thành chức năng Export DSNV ra Excel

--- ViewModels/StaffManagementVM/Feature/ExportExcel.cs
+++ ViewModels/StaffManagementVM/Feature/ExportExcel.cs
@@ -7,6 +7,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System.Data;
 
 namespace CineMajestic.ViewModels.StaffManagementVM
 {
@@ -19,8 +21,55 @@ namespace CineMajestic.ViewModels.StaffManagementVM
         }
         private void ExportExcel(object obj)
         {
-            string fileExcelName = "Custom.xlsx";
 
+            //chọn folder lưu file export
+            string Path = "";
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyComputer);
+            dialog.IsFolderPicker = true;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                Path = dialog.FileName;
+            }
+
+
+            string fileExcelName = "";//tên file export
+            string pathFile = "";//đường dẫn tuyệt đối của file export
+
+            while (true)
+            {
+                fileExcelName = "Staff_"+MotSoPTBoTro.RandomFileName() + ".xlsx";
+                pathFile= Path + @"\" + fileExcelName;
+                if(!File.Exists(pathFile))
+                {
+                    break;
+                }
+            }
+
+
+
+            //chuẩn bị  export
+
+            //tạo datatable tương ứng
+            DataTable table = new DataTable();
+            table.Columns.Add("ID", typeof(string));
+            table.Columns.Add("Họ và tên", typeof(string));
+            table.Columns.Add("Ngày sinh", typeof(string));
+            table.Columns.Add("Giới tính", typeof(string));
+            table.Columns.Add("SĐT", typeof(string));
+            table.Columns.Add("Email", typeof(string));
+            table.Columns.Add("Chức vụ", typeof(string));
+            table.Columns.Add("Lương", typeof(int));
+
+            
+            //chuyển list vào table
+            foreach(var item in FilterDSNV)
+            {
+                table.Rows.Add(item.IdFormat, item.FullName, item.Birth, item.Gender, item.PhoneNumber, item.Email, item.Role, item.Salary);
+            }
+
+
+            //tiến hành export
             try
             {
                 ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
@@ -28,11 +77,11 @@ namespace CineMajestic.ViewModels.StaffManagementVM
                 {
                     var worksheet = package.Workbook.Worksheets.Add("Sheet1");
 
-                    worksheet.Cells.LoadFromCollection(DSNV, true);
+                    worksheet.Cells.LoadFromDataTable(table, true);
 
                     worksheet.Cells[worksheet.Dimension.Address].AutoFitColumns();
 
-                    File.WriteAllBytes(fileExcelName, package.GetAsByteArray());
+                    File.WriteAllBytes(pathFile, package.GetAsByteArray());
                 }
                 MessageBox.Show("Thành công");
             }
