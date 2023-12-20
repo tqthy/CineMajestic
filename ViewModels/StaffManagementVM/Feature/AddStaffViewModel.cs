fc44b955337b43b8f185112e5c00bfc68ce30314
Dung Tien The <22520291@gm.uit.edu.vn>
2023-12-21 03:16:41 +0700

    QLNhanSu: Xử lý lỗi người dùng nhập phần addStaff (chưa xử lý phần password)

--- ViewModels/StaffManagementVM/Feature/AddStaffViewModel.cs
+++ ViewModels/StaffManagementVM/Feature/AddStaffViewModel.cs
@@ -39,17 +39,204 @@ namespace CineMajestic.ViewModels.StaffManagementVM
 
 
         //add cho bảng Staff
-        public string FullName { get; set; }
+
+        //họ và tên
+        private string fullName;
+        public string FullName
+        {
+            get => fullName;
+            set
+            {
+                fullName = value;
+                ValidateFullName();
+            }
+        }
+
+        private string fullNameError;
+        public string FullNameError
+        {
+            get => fullNameError;
+            set
+            {
+                fullNameError = value;
+                OnPropertyChanged(nameof(FullNameError));
+            }
+        }
+
+
+
+        //giới tính
         public string Gender { get; set; }
-        public DateTime? Birth { get; set; }
-        public string Email { get; set; }
-        public string PhoneNumber { get; set; }
+
+
+
+        //Ngày sinh(làm kiểu bắt lỗi cho ngày nhỏ hơn ngày hiện tại)
+        private DateTime? birth;
+        public DateTime? Birth
+        {
+            get => birth;
+            set
+            {
+                birth = value;
+                ValidateBirth();
+            }
+        }
+
+        private string birthError;
+        public string BirthError
+        {
+            get => birthError;
+            set
+            {
+                birthError = value;
+                OnPropertyChanged(nameof(BirthError));
+            }
+        }
+
+
+
+
+        //Email
+        private string email;
+        public string Email
+        {
+            get => email;
+            set
+            {
+                email = value;
+                ValidateEmail();
+            }
+        }
+        private string emailError;
+        public string EmailError
+        {
+            get => emailError;
+            set
+            {
+                emailError = value;
+                OnPropertyChanged(nameof(EmailError));
+            }
+        }
+
+
+
+
+        //Số điện thoại
+        private string phoneNumber;
+        public string PhoneNumber
+        {
+            get => phoneNumber;
+            set
+            {
+                phoneNumber = value;
+                ValidatePhoneNumber();
+            }
+        }
+
+        private string phoneNumberError;
+        public string PhoneNumberError
+        {
+            get => phoneNumberError;
+            set
+            {
+                phoneNumberError = value;
+                OnPropertyChanged(nameof(PhoneNumberError));
+            }
+        }
+
+
+
+
+        //Chức vụ
+
         public string Role { get; set; }
-        public DateTime? NgayVL { get; set; }
-        public int Salary { get; set; }
+
+
+
+        //ngày vào làm
+        private DateTime? ngayVL;
+        public DateTime? NgayVL
+        {
+            get => ngayVL;
+            set
+            {
+                ngayVL = value;
+                ValidateNgayVL();
+            }
+        }
+
+        private string ngayVLError;
+        public string NgayVLError
+        {
+            get => ngayVLError;
+            set
+            {
+                ngayVLError = value;
+                OnPropertyChanged(nameof(NgayVLError));
+            }
+        }
+
+
+
+
+
+        //lương
+        private string salary;
+        public string Salary
+        {
+            get => salary;
+            set
+            {
+                salary = value;
+                ValidateSalary();
+            }
+        }
+        private string salaryError;
+        public string SalaryError
+        {
+            get => salaryError;
+            set
+            {
+                salaryError = value;
+                OnPropertyChanged(nameof(SalaryError));
+            }
+        }
+
+
+
 
         //add bảng account
-        public string Username { get; set; }
+
+        //tên tài khoản
+        private string username;
+        public string Username
+        {
+            get => username;
+            set
+            {
+                username = value;
+                ValidateUsername();
+            }
+        }
+
+        private string usernameError;
+        public string UsernameError
+        {
+            get => usernameError;
+            set
+            {
+                usernameError = value;
+                OnPropertyChanged(nameof(UsernameError));
+            }
+        }
+
+
+       
+
+        //error nhập lại mật khẩu 
+
+
+
 
         public ICommand CancelCommand { get; set; }
         public ICommand acceptAddCommand { get; set; }//đồng ý thêm
@@ -60,6 +247,11 @@ namespace CineMajestic.ViewModels.StaffManagementVM
             this.wd = wd;
             CancelCommand = new ViewModelCommand(cancel);
             acceptAddCommand = new ViewModelCommand(accpetAdd);
+
+            Gender = "Nam";
+            Role = "Quản lý";
+            Birth = DateTime.UtcNow;
+            NgayVL = DateTime.UtcNow;
         }
         private void cancel(object obj)
         {
@@ -101,7 +293,7 @@ namespace CineMajestic.ViewModels.StaffManagementVM
 
             //add vào bảng Staff
             StaffDA staffDA = new StaffDA();
-            staffDA.addStaff(new StaffDTO(FullName, Birth.Value.ToString("yyyy-MM-dd"), Gender, Email, PhoneNumber, Salary, Role, NgayVL.Value.ToString("yyyy-MM-dd")));
+            staffDA.addStaff(new StaffDTO(FullName, Birth.Value.ToString("yyyy-MM-dd"), Gender, Email, PhoneNumber, int.Parse(Salary), Role, NgayVL.Value.ToString("yyyy-MM-dd")));
 
             //add vào bảng Account,sau này nhớ mã hóa pass ở đây luôn
             UserDA userDA = new UserDA();
@@ -112,5 +304,133 @@ namespace CineMajestic.ViewModels.StaffManagementVM
 
         }
 
+
+
+        //Các hàm Validate
+        private void ValidateFullName()
+        {
+            if (string.IsNullOrWhiteSpace(FullName))
+            {
+                FullNameError = "Họ và tên không được để trống!";
+            }
+            else
+            {
+                FullNameError = "";
+            }
+        }
+
+
+        private void ValidateBirth()
+        {
+            if (Birth>DateTime.UtcNow)
+            {
+                BirthError = "Ngày sinh không hợp lệ!";
+            }
+            else
+            {
+                BirthError = "";
+            }
+        }
+
+        private void ValidateEmail()
+        {
+            if (string.IsNullOrWhiteSpace(Email))
+            {
+                EmailError = "Email không được để trống!";
+            }
+            else if (!Email.Contains("@"))
+            {
+                EmailError = "Email không hợp lệ!";
+            }
+            else
+            {
+                EmailError = "";
+            }
+        }
+
+        private void ValidatePhoneNumber()
+        {
+            if (string.IsNullOrWhiteSpace(PhoneNumber))
+            {
+                PhoneNumberError = "SĐT không được để trống!";
+            }
+            else if (!PhoneNumber.All(char.IsDigit))
+            {
+                PhoneNumberError = "Số điện thoại chỉ được chứa chữ số!";
+            }
+            else
+            {
+                PhoneNumberError = "";
+            }
+        }
+
+
+        private void ValidateNgayVL()
+        {
+            if (NgayVL < Birth)
+            {
+                NgayVLError = "Ngày đăng ký phải lớn hơn ngày sinh!";
+            }
+            else
+            {
+                NgayVLError = "";
+            }
+        }
+
+
+        private void ValidateSalary()
+        {
+            if (string.IsNullOrWhiteSpace(Salary))
+            {
+                SalaryError = "Lương không hợp lệ!";
+            }
+            else if (!Salary.All(char.IsDigit))
+            {
+                SalaryError = "Lương không hợp lệ!";
+            }
+            else if (int.Parse(Salary) < 0)
+            {
+                SalaryError = "Lương không hợp lệ!";
+            }
+            else
+            {
+                SalaryError = "";
+            }
+        }
+
+
+        private void ValidateUsername()
+        {
+            if (string.IsNullOrWhiteSpace(Username))
+            {
+                UsernameError = "Username không được để trống!";
+            }
+            else if(Username.Contains(" "))
+            {
+                UsernameError = "Username không hợp lệ!";
+            }
+            else if (Username.Length < 6)
+            {
+                UsernameError = "Username phải lớn hơn 5 kí tự!";
+            }
+            else
+            {
+                UsernameError = "";
+            }
+        }
+
+
+
+        //mật khẩu
+        private void ValidatePassword1()
+        {
+
+        }
+
+        //nhập lại mật khẩu
+        private void ValidatePassword2()
+        {
+
+        }
     }
 }
