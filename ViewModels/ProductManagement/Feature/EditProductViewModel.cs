5fe8d73d0c974adf73d0b997e49f2829b72db896
Vu Binh <22520129@gm.uit.edu.vn>
2023-12-22 23:19:32 +0700

    Product-Edit: Báo lỗi đã hoàn thành

--- ViewModels/ProductManagement/Feature/EditProductViewModel.cs
+++ ViewModels/ProductManagement/Feature/EditProductViewModel.cs
@@ -39,8 +39,30 @@ namespace CineMajestic.ViewModels.ProductManagement
     public class EditProductViewModel:INotifyPropertyChanged
     {
         public ProductDTO productEdit {  get; set; }
-        public string Name { get; set; }
 
+        //Tên sản phẩm
+        private string name;
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                ValidateName();
+            }
+        }
+        private string nameError;
+        public string NameError
+        {
+            get => nameError;
+            set
+            {
+               nameError = value;
+               OnPropertyChanged(nameof(NameError));
+            }
+        }
+
+        //Ảnh
         private string imageSource;
         public string? ImageSource
         {
@@ -54,8 +76,50 @@ namespace CineMajestic.ViewModels.ProductManagement
                 }
             }
         }
-        public int Quantity { get; set; }
-        public int Price { get; set; }
+
+        //Quantity
+        private string quantity;
+        public string Quantity
+        {
+            get => quantity;
+            set
+            {
+                quantity = value;
+                ValidateQuantity();
+            }
+        }
+        private string quantityError;
+        public string QuantityError
+        {
+            get => quantityError;
+            set
+            {
+                quantityError = value;
+                OnPropertyChanged(nameof(QuantityError));
+            }
+        }
+
+        //Price
+        private string price;
+        public string Price
+        {
+            get => price;
+            set
+            {
+                price = value;
+                ValidatePrice();
+            }
+        }
+        private string priceError;
+        public string PriceError
+        {
+            get => priceError;
+            set
+            {
+                priceError = value;
+                OnPropertyChanged(nameof(PriceError));
+            }
+        }
         public int Type { get; set; }
 
         EditProduct wd;//phục vụ việc đóng wd
@@ -69,7 +133,7 @@ namespace CineMajestic.ViewModels.ProductManagement
         {
             quitCommand = new ViewModelCommand(quit);  
             addImageCommand=new ViewModelCommand(addImage);
-            acceptEditCommand = new ViewModelCommand(acceptEdit);
+            acceptEditCommand = new ViewModelCommand(acceptEdit,canAccept);
             WindowClosingCommand = new ViewModelCommand(windowClosing);
             this.wd = wd;
             deleteImageEdit();
@@ -78,8 +142,8 @@ namespace CineMajestic.ViewModels.ProductManagement
         public void khoitao()
         {
             Name = productEdit.Name;
-            Quantity=productEdit.Quantity;
-            Price = productEdit.Price;
+            Quantity=productEdit.Quantity.ToString();
+            Price = productEdit.Price.ToString();
             Type = productEdit.Type - 1;
             ImageSource= productEdit.ImageSource;
         }
@@ -95,11 +159,10 @@ namespace CineMajestic.ViewModels.ProductManagement
         private void acceptEdit(object obj)
         {
             string nameNew = Name;
-            int quantityNew = Quantity;
-            int priceNew= Price;
+            int quantityNew = int.Parse(Quantity);
+            int priceNew= int.Parse(Price);
             int typeNew = Type + 1;
             string imageSourceNew = Path.GetFileName(ImageSource);
-
             ProductDA productDA = new ProductDA();
             productDA.editProduct(new ProductDTO(productEdit.Id,nameNew, quantityNew, priceNew, typeNew, imageSourceNew));
             MessageBox.Show("Sửa thành công");
@@ -164,5 +227,74 @@ namespace CineMajestic.ViewModels.ProductManagement
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
+
+        //Các hàm Validate để báo lỗi
+
+        private bool [] _canAccept = new bool[3];
+        
+        private bool canAccept(object obj)
+        {
+            return _canAccept[0] && _canAccept[1] && _canAccept[2];
+        }
+        private void ValidateName()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                NameError = "Tên sản phẩm không được trống!";
+                _canAccept[0] = false;
+            }
+            else
+            {
+                NameError = "";
+                _canAccept[0] = true;
+            }
+        }
+
+        private void ValidateQuantity()
+        {
+            if (string.IsNullOrWhiteSpace(Quantity))
+            {
+                QuantityError = "Số lượng không để trống";
+                _canAccept[1] = false;
+            }
+            else if (!Quantity.All(char.IsDigit))
+            {
+                QuantityError = "Số lượng không hợp lệ";
+                _canAccept[1] = false;
+            }
+            else if (int.Parse(Quantity) < 0)
+            {
+                QuantityError = "Số lượng không hợp lệ";
+                _canAccept[1] = false;
+            }
+            else
+            {
+                QuantityError = "";
+                _canAccept[1] = true;
+            }
+        }
+        private void ValidatePrice()
+        {
+            if (string.IsNullOrWhiteSpace(Price))
+            {
+                PriceError = "Giá không để trống";
+                _canAccept[2] = false;
+            }
+            else if (!Price.All(char.IsDigit))
+            {
+                PriceError = "Giá không hợp lệ";
+                _canAccept[2] = false;
+            }
+            else if (int.Parse(Price) < 0)
+            {
+                PriceError = "Giá không hợp lệ";
+                _canAccept[2] = false;
+            }
+            else
+            {
+                PriceError = "";
+                _canAccept[2] = true;
+            }
+        }
     }
 }
