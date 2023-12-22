aeef0399e62f83a27b2fc139d2e5db49068eceeb
Dung Tien The <22520291@gm.uit.edu.vn>
2023-12-22 09:47:12 +0700

    InformationManagement: Tiến hành binding (với dữ liệu mẫu)

---
+++ ViewModels/InformationManagement/InformationViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineMajestic.ViewModels.InformationManagement
+{
+    public class InformationViewModel:MainBaseViewModel
+    {
+        public string FullName { get; set; }
+        public string Gender { get; set; }
+        public string Birth { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Role { get; set; }
+        public string NgayVL { get; set; }
+        public int Salary { get; set; }
+
+        private string imageSource;
+        public string ImageSource
+        {
+            get => imageSource;
+            set
+            {
+                imageSource = value;
+                OnPropertyChanged(nameof(ImageSource));
+            }
+        }
+        public InformationViewModel()
+        {
+            loadData();
+        }
+
+        private void loadData()
+        {
+            //tạm thời dùng dữ liệu mẫu vì chưa xử cái login
+            FullName = "Nguyễn Văn a";
+            Gender = "Nam";
+            Birth = "10/10/1999";
+            Email = "a@gmail.com";
+            PhoneNumber= "1234567890";
+            Role="Quản lý";
+            NgayVL = "12/12/2023";
+            Salary = 9999999;
+            ImageSource = "pack://application:,,,/Images/InformationManagement/Default.jpg";
+        }
+    }
+}
--- Views/InformationManagement/InformationView.xaml.cs
+++ Views/InformationManagement/InformationView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CineMajestic.ViewModels.InformationManagement;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,8 @@ namespace CineMajestic.Views.InformationManagement
         public InformationView()
         {
             InitializeComponent();
+            InformationViewModel informationViewModel = new InformationViewModel();
+            this.DataContext = informationViewModel;
         }
     }
 }
