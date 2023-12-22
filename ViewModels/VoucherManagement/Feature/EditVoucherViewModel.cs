604a893d1f746a521428028c23eb5e85a554897f
Vu Binh <22520129@gm.uit.edu.vn>
2023-12-22 10:32:43 +0700

    Voucher: Hoàn thành báo lỗi cho EditVoucher

--- ViewModels/VoucherManagement/Feature/EditVoucherViewModel.cs
+++ ViewModels/VoucherManagement/Feature/EditVoucherViewModel.cs
@@ -31,23 +31,105 @@ namespace CineMajestic.ViewModels.VoucherManagement
     }
 
 
-    public class EditVoucherViewModel
+    public class EditVoucherViewModel :MainBaseViewModel
     {
         private EditVoucher wd;
         public VoucherDTO voucher { get; set; }//voucher current
         public ICommand quitCommand { get; set; }
         public ICommand acceptCommand {  get; set; }
-        public string Name { get; set; }
-        public string VoucherDetail { get; set; }
-        public string Type { get; set; }
-        public DateTime? StartDate { get; set; }
-        public DateTime? FinDate { get; set; }
+        //Tên voucher
+        private string name;
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                ValidateName();
+            }
+        }
 
+        private string nameError;
+        public string NameError
+        {
+            get => nameError;
+            set
+            {
+                nameError = value;
+                OnPropertyChanged(nameof(NameError));
+            }
+        }
+        //Thông tin voucher
+        private string voucherDetail;
+        public string VoucherDetail
+        {
+            get => voucherDetail;
+            set
+            {
+                voucherDetail = value;
+                ValidateVoucherDetail();
+            }
+        }
+        private string voucherDetailError;
+        public string VoucherDetailError
+        {
+            get => voucherDetailError;
+            set
+            {
+                voucherDetailError = value;
+                OnPropertyChanged(nameof(VoucherDetailError));
+            }
+        }
+        public string Type { get; set; }
+        //Ngày bắt đầu ra voucher
+        private DateTime? startDate;
+        public DateTime? StartDate
+        {
+            get => startDate;
+            set
+            {
+                startDate = value;
+                ValidateStartDate();
+            }
+        }
+
+        private string startDateError;
+        public string StartDateError
+        {
+            get => startDateError;
+            set
+            {
+                startDateError = value;
+                OnPropertyChanged(nameof(StartDateError));
+            }
+        }
+        //Ngày voucher hết hạn
+        private DateTime? finDate;
+        public DateTime? FinDate
+        {
+            get => finDate;
+            set
+            {
+                finDate = value;
+                ValidateFinDate();
+            }
+        }
+
+        private string finDateError;
+        public string FinDateError
+        {
+            get => finDateError;
+            set
+            {
+                finDateError = value;
+                OnPropertyChanged(nameof(FinDateError));
+            }
+        }
         public EditVoucherViewModel(EditVoucher wd)
         {
             this.wd = wd;
             quitCommand=new ViewModelCommand(quit);
-            acceptCommand = new ViewModelCommand(accpet);
+            acceptCommand = new ViewModelCommand(accpet,canAcceptEdit);
         }
 
         public void loadEditCurrent()
@@ -76,6 +158,66 @@ namespace CineMajestic.ViewModels.VoucherManagement
             voucherDA.editVoucher(new VoucherDTO(voucher.Id, Name, VoucherDetail, Type, StartDate.Value.ToString("yyyy-MM-dd"), FinDate.Value.ToString("yyyy-MM-dd")));
             MessageBox.Show("Edit thành công");
             wd.Close();
+        }
+        private bool[] _canAccept = new bool[4];//phục vụ việc có cho nhấn button accept k
+        private bool canAcceptEdit(object obj)
+        {
+            return _canAccept[0] && _canAccept[1] && _canAccept[2] && _canAccept[3];
+        }
+
+        //Các hàm Validate
+        private void ValidateName()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                NameError = "Tên voucher không được để trống!";
+                _canAccept[0] = false;
+            }
+            else
+            {
+                NameError = "";
+                _canAccept[0] = true;
+            }
+        }
+        private void ValidateVoucherDetail()
+        {
+            if (string.IsNullOrWhiteSpace(VoucherDetail))
+            {
+                VoucherDetailError = "Thông tin voucher không được để trống!";
+                _canAccept[1] = false;
+            }
+            else
+            {
+                VoucherDetailError = "";
+                _canAccept[1] = true;
+            }
+        }
+        private void ValidateStartDate()
+        {
+            if (StartDate > FinDate || StartDate < DateTime.Now)
+            {
+                StartDateError = "Ngày ra voucher không hợp lệ";
+                _canAccept[2] = false;
+
+            }
+            else
+            {
+                StartDateError = "";
+                _canAccept[2] = true;
+            }
+        }
+        private void ValidateFinDate()
+        {
+            if (FinDate < StartDate)
+            {
+                FinDateError = "Ngày hết hạn voucher phải lớn hơn ngày ra";
+                _canAccept[3] = false;
+            }
+            else
+            {
+                FinDateError = "";
+                _canAccept[3] = true;
+            }
         }
     }
 }
