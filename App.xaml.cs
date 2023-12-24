0c5c1d70c58165278354f2d5865846f6486d7cdc
Dung Tien The <22520291@gm.uit.edu.vn>
2023-12-24 09:38:27 +0700

    Xử lý đụng độ khi Merge từ nhánh Product vào nhánh Information

--- App.xaml.cs
+++ App.xaml.cs
@@ -12,44 +12,12 @@ namespace CineMajestic
     /// </summary>
     public partial class App : Application
     {
-        //protected void ApplicationStart(object sender, EventArgs e)
-        //{
-        //    var loginView = new LoginView();
-        //    loginView.Show();
-        //    loginView.IsVisibleChanged += (s, ev) =>
-        //    {
-        //        if (loginView.IsVisible == false && loginView.IsLoaded)
-        //        {
-        //            var mainView = new MainView();
-        //            mainView.Show();
-        //            loginView.Close();
-        //        }
-        //    };
-        //}
-
-
-        //test
-        //private void Application_Startup(object sender, StartupEventArgs e)
-        //{
-        //    MainView mainView = new MainView();
-        //    mainView.Show();
-        //}
-
-
-
         private void ApplicationStart(object sender, EventArgs e)
         {
             LoginView loginView = new LoginView();
             loginView.Show();
         }
 
-
-        //test
-        private void Application_Startup(object sender, StartupEventArgs e)
-        {
-            MainView mainView = new MainView();
-            mainView.Show();
-        }
     }
 
 }
