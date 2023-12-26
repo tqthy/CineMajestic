fecefab27406d774d9e8c4b99fe1e7254ab263a9
justixz <22521461@gm.uit.edu.vn>
2023-12-26 13:47:20 +0700

    Add ErrorDTO

---
+++ Models/DTOs/ErrorDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineMajestic.Models.DTOs
+{
+    public class ErrorDTO
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Staff_Id { get; set; }
+        public string DateAdded { get; set; }
+        public string Status { get; set; }
+        public string StartDate { get; set; } 
+        public string EndDate { get; set; }
+        public string Cost { get; set; }
+    }
+}
