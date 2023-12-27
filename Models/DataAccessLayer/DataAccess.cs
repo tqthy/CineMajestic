5314400429a124999c17588821ec4802d7e524f6
Vu Binh <22520129@gm.uit.edu.vn>
2023-12-27 10:52:48 +0700

    Movie : Chỉnh lại connectstring

--- Models/DataAccessLayer/DataAccess.cs
+++ Models/DataAccessLayer/DataAccess.cs
@@ -14,9 +14,11 @@ namespace CineMajestic.Models.DataAccessLayer
         {
             // _connectionString = "server=tcp:192.168.0.199,1433;database=CinemaManagement;user id=sa;password=Password.1";
             //_connectionString = "Server=tcp:172.16.0.71,1433;Database=CinemaManagement;User ID=sa;Password=Password.1";
-            //_connectionString = @"Data Source=DESKTOP-34OSP4G\SQLEXPRESS;Initial Catalog=QL_RapChieuPhim;Integrated Security=True";
-            // _connectionString = @"Data Source=LAPTOP-J7OECDJF\SQLEXPRESS;Initial Catalog=CinemaManagement;Integrated Security=True";
-            _connectionString = @"Data Source=DESKTOP-34OSP4G\SQLEXPRESS;Initial Catalog=CineMajestic;Integrated Security=True";
+            
+            //Dũng
+            //_connectionString = @"Data Source=DESKTOP-34OSP4G\SQLEXPRESS;Initial Catalog=CineMajestic;Integrated Security=True";
+            //Bình
+            _connectionString = @"Data Source=LAPTOP-J7OECDJF\SQLEXPRESS;Initial Catalog=CineMajestic;Integrated Security=True";
         }
         protected SqlConnection GetConnection()
         {
