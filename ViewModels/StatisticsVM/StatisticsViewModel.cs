daeb456ca78a111110be4a6cd56e4c1da57cb6ab
justixz <22521461@gm.uit.edu.vn>
2023-12-27 16:44:02 +0700

    Reinstall LiveCharts

---
+++ ViewModels/StatisticsVM/StatisticsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+
+namespace CineMajestic.ViewModels.StatisticsVM
+{
+    public class StatisticsViewModel
+    {
+
+    }
+}
