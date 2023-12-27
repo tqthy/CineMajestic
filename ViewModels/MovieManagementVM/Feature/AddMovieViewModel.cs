70aa87aad467d13ee3f9d7105936aae1c48882d5
Vu Binh <22520129@gm.uit.edu.vn>
2023-12-27 12:53:33 +0700

    Movie: Thêm sửa lỗi người dùng ở addmovie

--- ViewModels/MovieManagementVM/Feature/AddMovieViewModel.cs
+++ ViewModels/MovieManagementVM/Feature/AddMovieViewModel.cs
@@ -12,6 +12,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using System.Xml.Linq;
 
 namespace CineMajestic.ViewModels.MovieManagementVM
 {
@@ -43,9 +44,21 @@ namespace CineMajestic.ViewModels.MovieManagementVM
             set
             {
                 title = value;
-                OnPropertyChanged(nameof(Title));
+                ValidateTitle();
+
             }
         }
+        private string titleError;
+        public string TitleError
+        {
+            get => titleError;
+            set
+            {
+                titleError = value;
+                OnPropertyChanged(nameof(TitleError));
+            }
+        }
+
 
 
         //chi tiết
@@ -56,11 +69,21 @@ namespace CineMajestic.ViewModels.MovieManagementVM
             set
             {
                 description= value;
-                OnPropertyChanged(nameof(Description));
+                ValidateDescription();
+
             }
         }
 
-
+        private string descriptionError;
+        public string DescriptionError
+        {
+            get => descriptionError;
+            set
+            {
+                descriptionError = value;
+                OnPropertyChanged(nameof(DescriptionError));
+            }
+        }
         //thể loại
         private string genre;
         public string Genre
@@ -69,7 +92,17 @@ namespace CineMajestic.ViewModels.MovieManagementVM
             set
             {
                 genre= value;
-                OnPropertyChanged(nameof(Genre));
+                ValidateGenre();
+            }
+        }
+        private string genreError;
+        public string GenreError
+        {
+            get => genreError;
+            set
+            {
+                genreError = value;
+                OnPropertyChanged(nameof(GenreError));
             }
         }
 
@@ -81,7 +114,17 @@ namespace CineMajestic.ViewModels.MovieManagementVM
             set
             {
                 director= value;
-                OnPropertyChanged(nameof(Director));
+                ValidateDirector();
+            }
+        }
+        private string directorError;
+        public string DirectorError
+        {
+            get => directorError;
+            set
+            {
+                directorError = value;
+                OnPropertyChanged(nameof(DirectorError));
             }
         }
 
@@ -94,10 +137,20 @@ namespace CineMajestic.ViewModels.MovieManagementVM
             set
             {
                 releaseYear = value;
-                OnPropertyChanged(nameof (ReleaseYear));
+                ValidateReleaseYear();
             }
         }
 
+        private string releaseYearError;
+        public string ReleaseYearError
+        {
+            get => releaseYearError;
+            set
+            {
+                releaseYearError = value;
+                OnPropertyChanged(nameof(ReleaseYearError));
+            }
+        }
 
         //ngôn ngữ
         private string language;
@@ -107,7 +160,17 @@ namespace CineMajestic.ViewModels.MovieManagementVM
             set
             {
                 language= value;
-                OnPropertyChanged(nameof(Language));
+                ValidateLanguage();
+            }
+        }
+        private string languageError;
+        public string LanguageError
+        {
+            get => languageError;
+            set
+            {
+                languageError = value;
+                OnPropertyChanged(nameof(LanguageError));
             }
         }
 
@@ -120,7 +183,18 @@ namespace CineMajestic.ViewModels.MovieManagementVM
             set
             {
                 country = value;
-                OnPropertyChanged(nameof(Country));
+                ValidateCountry();
+            }
+        }
+
+        private string countryError;
+        public string CountryError
+        {
+            get => countryError;
+            set
+            {
+                countryError = value;
+                OnPropertyChanged(nameof(CountryError));
             }
         }
 
@@ -134,10 +208,19 @@ namespace CineMajestic.ViewModels.MovieManagementVM
             set
             {
                 length= value;
-                OnPropertyChanged(nameof(Length));
+                ValidateLength();
             }
         }
-
+        private string lengthError;
+        public string LengthError
+        {
+            get => lengthError;
+            set
+            {
+                lengthError = value;
+                OnPropertyChanged(nameof(LengthError));
+            }
+        }
 
         //trailer
         private string trailer;
@@ -147,7 +230,17 @@ namespace CineMajestic.ViewModels.MovieManagementVM
             set
             {
                 trailer= value;
-                OnPropertyChanged(nameof(Trailer));
+                ValidateTrailer();
+            }
+        }
+        private string trailerError;
+        public string TrailerError
+        {
+            get => trailerError;
+            set
+            {
+                trailerError = value;
+                OnPropertyChanged(nameof(TrailerError));
             }
         }
 
@@ -160,10 +253,11 @@ namespace CineMajestic.ViewModels.MovieManagementVM
             set
             {
                 startDate = value;
-                OnPropertyChanged(nameof(StartDate));
+                OnPropertyChanged(nameof(startDate));
             }
         }
 
+        
 
         //trạng thái
         private string status;
@@ -172,11 +266,14 @@ namespace CineMajestic.ViewModels.MovieManagementVM
             get => status;
             set
             {
-                status= value;
-                OnPropertyChanged(nameof(Status));
+                status = value;
+                OnPropertyChanged(nameof(Status));            
             }
         }
 
+        
+
+        //Giá nhập
         private string importPrice;
         public string ImportPrice
         {
@@ -184,7 +281,17 @@ namespace CineMajestic.ViewModels.MovieManagementVM
             set
             {
                 importPrice= value;
-                OnPropertyChanged(nameof(ImportPrice));
+                ValidateImportPrice();
+            }
+        }
+        private string importPriceError;
+        public string ImportPriceError
+        {
+            get => importPriceError;
+            set
+            {
+                importPriceError = value;
+                OnPropertyChanged(nameof(ImportPriceError));
             }
         }
 
@@ -211,9 +318,11 @@ namespace CineMajestic.ViewModels.MovieManagementVM
         private AddMovieView addMovieView;
         public AddMovieViewModel(AddMovieView addMovieView)
         {
-            acceptCommand = new ViewModelCommand(accept);
+            acceptCommand = new ViewModelCommand(accept,canAcceptAdd);
             addImageCommand = new ViewModelCommand(addImage);
             this.addMovieView = addMovieView;
+            StartDate = DateTime.Now;
+            Status = "Đang phát hành";
         }
         private void accept(object obj)
         {
@@ -277,6 +386,181 @@ namespace CineMajestic.ViewModels.MovieManagementVM
                 catch { }
             }
         }
+
+        //Phần các hàm validate _ báo lỗi
+        private bool[] _canAccept = new bool[10];//phục vụ việc có cho nhấn button accept ko
+        private bool canAcceptAdd(object obj)
+        {
+            return _canAccept[0] && _canAccept[1] && _canAccept[2] && _canAccept[3] && _canAccept[4] && _canAccept[5] &&
+                _canAccept[6] && _canAccept[7] && _canAccept[8] && _canAccept[9];
+        }
+        private void ValidateTitle()
+        {
+            if (string.IsNullOrWhiteSpace(Title))
+            {
+                TitleError = "Không để trống!";
+                _canAccept[0] = false;
+            }
+            else
+            {
+                TitleError = "";
+                _canAccept[0] = true;
+            }
+        }
+
+        private void ValidateDescription()
+        {
+            if (string.IsNullOrWhiteSpace(Description))
+            {
+                DescriptionError = "Không để trống!";
+                _canAccept[1] = false;
+            }
+            else
+            {
+                DescriptionError = "";
+                _canAccept[1] = true;
+            }
+        }
+        private void ValidateGenre()
+        {
+            if (string.IsNullOrWhiteSpace(Genre))
+            {
+                GenreError = "Không để trống!";
+                _canAccept[2] = false;
+            }
+            else
+            {
+                GenreError = "";
+                _canAccept[2] = true;
+            }
+        }
+
+        private void ValidateDirector()
+        {
+            if (string.IsNullOrWhiteSpace(Director))
+            {
+                DirectorError = "Không để trống!";
+                _canAccept[3] = false;
+            }
+            else
+            {
+                DirectorError = "";
+                _canAccept[3] = true;
+            }
+        }
+
+        private void ValidateLength()
+        {
+            if (string.IsNullOrWhiteSpace(Length))
+            {
+                LengthError = "Không để trống!";
+                _canAccept[4] = false;
+            }
+            else if (!Length.All(char.IsDigit))
+            {
+                LengthError = "Không hợp lệ!";
+                _canAccept[4] = false;
+            }
+            else if (int.Parse(Length) < 0)
+            {
+                LengthError = "Không hợp lệ!";
+                _canAccept[4] = false;
+            }
+            else
+            {
+                LengthError = "";
+                _canAccept[4] = true;
+            }
+        }
+
+        private void ValidateLanguage()
+        {
+            if (string.IsNullOrWhiteSpace(Language))
+            {
+                LanguageError = "Không để trống!";
+                _canAccept[5] = false;
+            }
+            else
+            {
+                LanguageError = "";
+                _canAccept[5] = true;
+            }
+        }
+
+        private void ValidateCountry()
+        {
+            if (string.IsNullOrWhiteSpace(Country))
+            {
+                CountryError = "Không để trống!";
+                _canAccept[6] = false;
+            }
+            else
+            {
+                CountryError = "";
+                _canAccept[6] = true;
+            }
+        }
+
+        private void ValidateTrailer()
+        {
+            if (string.IsNullOrWhiteSpace(Trailer))
+            {
+                TrailerError = "Không để trống!";
+                _canAccept[7] = false;
+            }
+            else
+            {
+                TrailerError = "";
+                _canAccept[7] = true;
+            }
+        }
+        private void ValidateReleaseYear()
+        {
+            if (string.IsNullOrWhiteSpace(ReleaseYear))
+            {
+                ReleaseYearError = "Không để trống!";
+                _canAccept[8] = false;
+            }
+            else if (!ReleaseYear.All(char.IsDigit))
+            {
+                ReleaseYearError = "Không hợp lệ!";
+                _canAccept[8] = false;
+            }
+            else if (int.Parse(ReleaseYear) < 0)
+            {
+                ReleaseYearError = "Không hợp lệ!";
+                _canAccept[8] = false;
+            }
+            else
+            {
+                ReleaseYearError = "";
+                _canAccept[8] = true;
+            }
+        }
+        private void ValidateImportPrice()
+        {
+            if (string.IsNullOrWhiteSpace(ImportPrice))
+            {
+                ImportPriceError = "Không để trống!";
+                _canAccept[9] = false;
+            }
+            else if (!ImportPrice.All(char.IsDigit))
+            {
+                ImportPriceError = "Không hợp lệ!";
+                _canAccept[9] = false;
+            }
+            else if (int.Parse(ImportPrice) < 0)
+            {
+                ImportPriceError = "Không hợp lệ!";
+                _canAccept[9] = false;
+            }
+            else
+            {
+                ImportPriceError = "";
+                _canAccept[9] = true;
+            }
+        }
+
 
 
     }
