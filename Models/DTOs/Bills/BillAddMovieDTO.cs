85435de5ea7aa0b562df80f47557302021a59f31
justixz <22521461@gm.uit.edu.vn>
2023-12-28 11:50:30 +0700

    Add Bills DTO

---
+++ Models/DTOs/Bills/BillAddMovieDTO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineMajestic.Models.DTOs
+{
+    public class BillAddMovieDTO
+    {
+        public int Id { get; set; }
+        public int Movie_Id { get; set; }
+        public string BillDate { get; set; }
+        public int Total { get; set; }
+
+
+        public BillAddMovieDTO(int id, int movie_Id, string billDate, int total)
+        {
+            Id = id;
+            Movie_Id = movie_Id;
+            BillDate = billDate;
+            Total = total;
+        }
+
+        //hỗ trợ add
+        public BillAddMovieDTO(int movie_Id, string billDate, int total)
+        {
+            Movie_Id = movie_Id;
+            BillDate = billDate;
+            Total = total;
+        }
+    }
+}
---
+++ Models/DTOs/Bills/BillAddProductDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineMajestic.Models.DTOs.Bills
+{
+    public class BillAddProductDTO
+    {
+        public int Id { get; set; }
+        public string BillDate { get; set; }
+        public int Total { get; set; }
+    }
+}
