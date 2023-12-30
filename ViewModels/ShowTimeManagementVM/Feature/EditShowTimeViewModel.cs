da399d849fab31e77447bfd37ad7e5c72f040aad
Vu Binh <22520129@gm.uit.edu.vn>
2023-12-31 00:22:47 +0700

    ShowTIme; Tạo command mở window edit lên

---
+++ ViewModels/ShowTimeManagementVM/Feature/EditShowTimeViewModel.cs
@@ -0,0 +1,26 @@
+﻿using CineMajestic.Views.ShowTimeManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CineMajestic.ViewModels.ShowTimeManagementVM
+{
+    public partial class ShowTimeManagementViewModel
+    {
+        public ICommand showEditView {  get; set; }
+
+        void edit()
+        {
+            showEditView = new ViewModelCommand(showEdit);
+        }
+        private void showEdit(object obj)
+        {
+            EditShowTimeView view = new EditShowTimeView();
+            view.ShowDialog();
+            loadData();
+        }
+    }
+}
