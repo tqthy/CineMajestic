b087d148b1c267d7f90eb99f3717e52cd4e3b715
Dung Tien The <22520291@gm.uit.edu.vn>
2023-12-31 16:04:24 +0700

    Done tính năng lọc suất chiếu theo ngày

--- ViewModels/ShowTimeManagementVM/Feature/FilterShowtime.cs
+++ ViewModels/ShowTimeManagementVM/Feature/FilterShowtime.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using CineMajestic.Models.DTOs.ShowTimeManagement;
+using Microsoft.Xaml.Behaviors.Media;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +25,16 @@ namespace CineMajestic.ViewModels.ShowTimeManagementVM
                 if (filterStartDate != null)
                 {
                     dateChoice = filterStartDate.Value.ToString("dd/MM/yyyy");
+                    ObservableCollection<ShowTimeDTO> list = new ObservableCollection<ShowTimeDTO>();
+                    foreach(var item in DSSuatChieu)
+                    {
+                        string[]s=item.StartTime.Split(' ');
+                        if (s[0] == dateChoice)
+                        {
+                            list.Add(item);
+                        }
+                    }
+                    DSSuatChieu=list;
                 }
             }
         }
