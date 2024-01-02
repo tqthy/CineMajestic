f09112b6a450c93b73ad3268122c221e45dacfba
Dung Tien The <22520291@gm.uit.edu.vn>
2024-01-02 20:55:58 +0700

    up csdl

--- CinemaManagement.sql
+++ CinemaManagement.sql
@@ -1,11 +1,11 @@
 ﻿--tạo database
 go
-create database CineMajestic
+create database CineMajesticImage
 go
 
 --sử dụng database
 go
-use CineMajestic
+use CineMajesticImage
 go
 
 
@@ -22,7 +22,7 @@ create table Staff
 	Salary int not null,
 	Role nvarchar(30) not null,
 	NgayVaolam smalldatetime not null,
-	ImageSource varchar(50) default 'Default.jpg'
+	ImageSource varbinary(max),
 )
 go
 --tân từ cho bảng Staff: mỗi nhân viên có id riêng làm khóa chính để phân biệt, có họ tên,ngày sinh,giới tính,email,sđt,mức lương,chức vụ,ngày vào làm,ảnh thẻ(sẽ dùng ảnh mặc định khi nhân viên vừa vào làm)
@@ -77,7 +77,6 @@ go
 --tân từ: mỗi khách hàng có id riêng làm khóa chính để phân biệt, có tên,sđt,email,point,ngày sinh,ngày đăng ký,và giới tính
 
 
-
 --bảng MOVIES
 GO
 CREATE TABLE MOVIE
@@ -95,19 +94,11 @@ CREATE TABLE MOVIE
     StartDate SMALLDATETIME not null,
     Status nvarchar(50) not null,
 	ImportPrice int not null,
-    ImageSource varchar(100) not null,
+    ImageSource varbinary(max) not null,
 );
 GO
 
---insert into MOVIE(Title,Description,Genre,Director,ReleaseYear,Language,Country,Length,Trailer,StartDate,Status,ImportPrice,ImageSource)
---values(N'Bố già',N'Phim hay',N'Kịch tính',N'Trần Thành',2022,N'Tiếng Việt',N'Việt Nam',120,'youtube','2022-12-31',N'Đang phát hành',1000000,'bogia.jpg')
 
---update MOVIE
---set ImageSource='bogia.jpg'
---where ID=1
-
-
---select * from MOVIE
 --tân từ: mỗi bộ phim có 1 id riêng để phân biệt với các bộ phim khác,có tiêu đề,miêu tả về phim,thể loại phim,đạo diễn 
 --năm ra mắt,ngôn ngữ trong phim,quốc gia sx,thời lượng phim(phút),link trailer,ngày ra mắt phim,trạng thái phim trong rạp(đang phát hành,ngừng ph),đường dẫn ảnh phim
 
@@ -119,7 +110,7 @@ create table Product
 (
 	ID int identity(1,1) primary key,
 	Name nvarchar(100) not null,
-	ImageSource varchar(200) not null,
+	ImageSource varbinary(max) not null,
 	Quantity int not null,
 	PurchasePrice int not null,
 	Price int null,
@@ -127,27 +118,6 @@ create table Product
 )
 go
 --tân từ: mỗi product sẽ có id riêng làm khóa chính để phân biệt với các product khác,có tên,đường dẫn ảnh,số lượng hiện có,giá nhập vào,giá bán(sẽ được tự động set=1.2 giá nhập),loại product(thức ăn ,đồ uống)
-
---trigger tự set giá sau mỗi lần update,insert: giá bán = giá nhập +giá nhập *20%
-go
-create trigger trg_setPrice_Product
-on Product
-for insert,update
-as
-begin
-	declare @ID int
-	declare @PurchasePrice int
-
-	select @ID=ID, @PurchasePrice=PurchasePrice
-	from inserted
-
-	update Product
-	set Price=@PurchasePrice+0.2*@PurchasePrice
-	where ID=@ID
-end
-go
-
-
 
 
 
@@ -185,7 +155,6 @@ values(N'Phòng 7',176)
 
 
 
---select * from Auditorium
 
 
 --bảng chỗ ngồi
@@ -265,13 +234,11 @@ execute sp_generate_seat_for_auditorium 6
 execute sp_generate_seat_for_auditorium 7
 go
 
---select * from Seat
 
 
 
 
 
---tạo trigger khi insert 1 showtime thì phải tạo dữ liệu cho bảng(tạo ở dưới bảng create showtime)
 
 
 --bảng suất chiếu
@@ -294,25 +261,7 @@ go
 --cho biết phòng nào đang chiếu
 
 
---trigger khi insert 1 showtime thì tạo dữ liệu cho bảng SeatForShowtime
-go
-create trigger trg_createSeatForShowtimeByShowtime_insert
-on ShowTime
-after insert
-as
-begin
-	declare @ShowtimeId int
-	declare @Auditorium_Id int
 
-	select @ShowtimeId=Id,@Auditorium_Id=Auditorium_Id
-	from inserted
-
-	insert into SeatForShowtime(Seat_Id,ShowTime_Id,Condition)
-	select Id, @ShowtimeId, 0
-    from Seat
-    where Auditorium_Id = @Auditorium_Id
-end
-go
 
 --chỗ ngồi cho từng showtime: khi add 1 showtime phải add dữ liệu cho bảng này
 --xóa showtime thì cái này xóa
@@ -328,20 +277,6 @@ create table SeatForShowtime
 )
 go
 
---select SeatForShowtime.Id as SeatForShowTime_Id,SeatForShowtime.Seat_Id as Seat_Id,ShowTime_Id,Location,Condition
---from SeatForShowtime
---inner join Seat on Seat.Id=SeatForShowtime.Id
---where SeatForShowtime.ShowTime_Id=8
-
---select * from MOVIE
-
---select * from ShowTime
-
---select Showtime.Id as ShowTimeId,ShowTime.StartTime,ShowTime.EndTime,ShowTime.PerSeatTicketPrice,Movie.id as MovieId,Movie.Length,Movie.Title,Auditorium.Name as phongchieu
---from ShowTime 
---inner join Auditorium on ShowTime.Auditorium_Id=Auditorium.Id
---inner join MOVIE on ShowTime.Movie_Id=MOVIE.id
---where Auditorium.Name=N'Phòng 1'
 
 
 
@@ -375,20 +310,6 @@ go
 --giá trị hóa đơn
 
 
---bảng vé
-go
-create table Ticket
-(
-	Id int identity(1,1) primary key,
-	Seat_Id int not null,
-	Bill_Id int not null,
-	Constraint FK_Seat_Id_ByTicket foreign key(Seat_Id) references Seat(Id),
-	Constraint FK_Bill_Id_ByTicket foreign key(Bill_Id) references Bill(Id),
-)
-go
---mỗi vé có id riêng làm khóa chính để phân biệt với vé khác
---cho biết vé thuộc hóa đơn nào(từ đó biết được ai mua,suất chiếu gì,phòng nào)
---cho biết chỗ ngồi của vé
 
 
 
@@ -427,26 +348,6 @@ create table Bill_AddMovie
 go
 
 
---trigger cập nhật lại billaddmovie khi edit movie
-go
-create trigger trg_updateBillAddmovie_update
-on Movie
-after update
-as
-begin
-	declare @MovieId int
-	declare @ImportPrice int
-
-	select @MovieId=Id,@ImportPrice=ImportPrice
-	from inserted
-
-	update Bill_AddMovie
-	set Total=@ImportPrice
-	where Id=@MovieId
-end
-go
-
---select * from Bill_AddMovie
 
 --bill nhập 1 product
 go
@@ -477,7 +378,9 @@ create table Bill_ImportProduct
 go
 
 
--- error
+
+--bảng error
+go
 CREATE TABLE ERRORS(
     id INT IDENTITY(1, 1) PRIMARY KEY,
     NAME NVARCHAR(100) NOT NULL,
@@ -487,5 +390,287 @@ CREATE TABLE ERRORS(
     ENDDATE SMALLDATETIME,
     COST INT,
     STAFF_id INT CONSTRAINT FK_ERR_STAFF FOREIGN KEY REFERENCES STAFF(Id),
-    IMAGE NVARCHAR(100) NOT NULL
+    ImageSource varbinary(max) not null,
 )
+go
+
+
+
+--bảng lương của staff
+go
+create table Staff_Salary (
+	Id int primary key IDENTITY(1, 1),
+	Staff_Id INT,
+	BillDate DATETIME,
+	Total INT,
+	CONSTRAINT FK_Salary_Staff_Id FOREIGN KEY (Staff_Id) REFERENCES Staff(Id)
+)
+go
+--store procedure hỗ trợ phát lương
+go
+create procedure sp_phatluong_staff_salary
+as
+begin
+    declare @BillDate smalldatetime
+    set @BillDate= GETDATE()
+
+    insert into Staff_Salary(Staff_Id, BillDate, Total)
+    select Id, @BillDate, Salary
+    from Staff
+end
+go
+
+
+
+insert into Staff(FullName,Birth,Gender,Email,PhoneNumber,Salary,Role,NgayVaolam)
+values('admin','1950-01-01','Nam','hihi@gmail.com','43302043',2000000000,N'Quản lý','2000-01-01')
+
+
+
+insert into ACCOUNTS(Username,Password,Staff_Id)
+values('admin','e10adc3949ba59abbe56e057f20f883e',1)
+
+
+
+--trigger khi xóa 1 staff
+go
+create trigger trg_DeleteStaff
+on staff
+instead of delete
+as
+begin
+	declare @staffid int
+
+	select @staffid=id
+	from deleted
+
+	delete ACCOUNTS
+	where Staff_Id=@staffid
+
+	update Staff_Salary
+	set Staff_Id=null
+	where Staff_Id=@staffid
+
+	update ERRORS
+	set STAFF_id=null
+	where STAFF_id=@staffid
+
+	update Bill
+	set Staff_Id=null
+	where Staff_Id=@staffid
+
+	delete Staff
+	where id=@staffid
+
+end
+go
+
+
+--trigger hỗ trợ những việc cần làm khi xóa 1 khách hàng
+go
+create trigger trg_Customer_Delete
+on customer
+instead of delete
+as
+begin
+	declare @Customer_Id int
+
+	select @Customer_Id=Id
+	from deleted
+
+	update Bill
+	set Customer_Id=null
+	where Customer_Id=@Customer_Id
+
+	delete CUSTOMER
+	where Id=@Customer_Id
+end
+go
+
+
+--trigger hỗ trợ điều sẽ xảy ra nếu xóa 1 movie
+go
+create trigger trg_Delete_Movie
+on Movie
+instead of delete
+as
+begin
+	declare @Movie_Id int
+
+	select @Movie_Id=id
+	from deleted
+
+	update Bill_AddMovie
+	set Movie_Id=null
+	where Movie_Id=@Movie_Id
+
+	delete ShowTime
+	where Movie_Id=@Movie_Id
+
+	delete MOVIE
+	where id=@Movie_Id
+end
+go
+
+
+--trigger tự set giá sau mỗi lần update,insert: giá bán = giá nhập +giá nhập *20%
+go
+create trigger trg_setPrice_Product
+on Product
+for insert
+as
+begin
+	declare @ID int
+	declare @PurchasePrice int
+
+	select @ID=ID, @PurchasePrice=PurchasePrice
+	from inserted
+
+	update Product
+	set Price=@PurchasePrice+0.2*@PurchasePrice
+	where ID=@ID
+end
+go
+
+--trigger giúp tự  add bill add product
+go
+create trigger trg_autoAddBill_AddProduct
+on Product
+after insert
+as
+begin
+	declare @Product_Id int
+	declare @Quantity int
+	declare @BillDate smalldatetime
+	declare @UnitPurchasePrice int
+
+	select @Product_Id=ID,@Quantity=Quantity,@UnitPurchasePrice=PurchasePrice
+	from inserted
+
+	set @BillDate= GETDATE()
+
+	insert into Bill_AddProduct(Product_Id,BillDate,Quantity,UnitPurchasePrice)
+	values(@Product_Id,@BillDate,@Quantity,@UnitPurchasePrice)
+end
+go
+
+--trigger giúp tự add bill import product
+go
+create trigger trg_autoAddBill_ImportProduct
+on product
+after update
+as
+begin
+	declare @Id int
+	declare @QuantityNew int
+	declare @UnitPuchasePrice int
+	declare @QuantityOld int
+
+	select @Id=ID,@QuantityOld=Quantity
+	from deleted
+
+	select @QuantityNew=Quantity,@UnitPuchasePrice=PurchasePrice
+	from inserted
+
+	declare @denta int
+	set @denta=@QuantityNew-@QuantityOld
+
+	insert into Bill_ImportProduct(Product_Id,BillDate,Quantity,UnitPurchasePrice)
+	values(@Id,GETDATE(),@denta,@UnitPuchasePrice)
+end
+go
+
+--trigger hỗ trợ điều sẽ xảy ra nếu xóa 1 product
+go
+create trigger trg_deleteProduct
+on product
+instead of delete
+as
+begin
+	declare @Product_Id int
+
+	select @Product_Id=id
+	from deleted
+
+	update Bill_ImportProduct
+	set Product_Id=null
+	where Product_Id=@Product_Id
+
+	update BillDetail
+	set Product_Id=null
+	where Product_Id=@Product_Id
+
+	update Bill_AddProduct
+	set Product_Id=null
+	where Product_Id=@Product_Id
+
+	delete Product
+	where ID=@Product_Id
+end
+go
+
+
+--trigger khi insert 1 showtime thì tạo dữ liệu cho bảng SeatForShowtime
+go
+create trigger trg_createSeatForShowtimeByShowtime_insert
+on ShowTime
+after insert
+as
+begin
+	declare @ShowtimeId int
+	declare @Auditorium_Id int
+
+	select @ShowtimeId=Id,@Auditorium_Id=Auditorium_Id
+	from inserted
+
+	insert into SeatForShowtime(Seat_Id,ShowTime_Id,Condition)
+	select Id, @ShowtimeId, 0
+    from Seat
+    where Auditorium_Id = @Auditorium_Id
+end
+go
+
+
+--trigger hỗ trợ những việc cần làm nếu xóa 1 showtime
+go
+create trigger trg_deleteShowtime
+on ShowTime
+instead of delete
+as
+begin
+	declare @ShowtimeId int
+	select @ShowtimeId=Id
+	from deleted
+
+	delete SeatForShowtime
+	where ShowTime_Id=@ShowtimeId
+
+	update Bill
+	set ShowTime_Id=null
+	where ShowTime_Id=@ShowtimeId
+
+	delete ShowTime
+	where Id=@ShowtimeId
+end
+go
+
+
+--trigger cập nhật lại billaddmovie khi edit movie
+go
+create trigger trg_updateBillAddmovie_update
+on Movie
+after update
+as
+begin
+	declare @MovieId int
+	declare @ImportPrice int
+
+	select @MovieId=Id,@ImportPrice=ImportPrice
+	from inserted
+
+	update Bill_AddMovie
+	set Total=@ImportPrice
+	where Id=@MovieId
+end
+go
+
