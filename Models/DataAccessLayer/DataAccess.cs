4fb5b1e302b4d692ccf2476844e9c90ddb6af6aa
Vu Binh <22520129@gm.uit.edu.vn>
2024-01-02 10:23:23 +0700

    Database: Chỉnh connectstring cho DataAccess với thêm vào App.config

--- Models/DataAccessLayer/DataAccess.cs
+++ Models/DataAccessLayer/DataAccess.cs
@@ -13,15 +13,8 @@ namespace CineMajestic.Models.DataAccessLayer
         private readonly string _connectionString;
         public DataAccess()
         {
-            //_connectionString = "Server=tcp:192.168.0.199,1433;Database=CineMajestic;User ID=sa;Password=Password.1";
-            //_connectionString = "Server=tcp:172.16.0.71,1433;Database=CinemaManagement;User ID=sa;Password=Password.1";
-            //            _connectionString = "Server=tcp:cinemajestic.database.windows.net,1433;Initial Catalog=Cinemajestic;Persist Security Info=False;User ID=cinemajestic;Password=Binh@2k4;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-            //Dũng
-            //_connectionString = @"Data Source=DESKTOP-34OSP4G\SQLEXPRESS;Initial Catalog=CineMajestic;Integrated Security=True";
-            //Bình
-            //_connectionString = @"Data Source=LAPTOP-J7OECDJF\SQLEXPRESS;Initial Catalog=CineMajestic;Integrated Security=True";
-
-            _connectionString = ConfigurationManager.ConnectionStrings["MyDBConnectionString"].ConnectionString;
+            _connectionString = "Server=tcp:cinemajestic.database.windows.net,1433;Initial Catalog=Cinemajestic;Persist Security Info=False;User ID=cinemajestic;Password=Binh@2k4;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            //_connectionString = ConfigurationManager.ConnectionStrings["MyDBConnectionString"].ConnectionString;
 
         }
 
